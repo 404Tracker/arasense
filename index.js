b3ba41512642a7140b69200bb1df86d034841198
Mohamad Alfaifi <muhamad.alfaifi@gmail.com>
2017-10-11 09:42:45 +0300

    talking between python and node

--- index.js
+++ index.js
@@ -2,42 +2,44 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const pythonShell = require('python-shell');
+
+let userId = 'SuYaVmIwLjsjhwnMIoYhkobxFUZvQXVOXODcL6cYgp4WAX7ZNr';
+pythonShell.run('./pickTweet.py', { args: userId }, function(err, ...results) {
+  if (err) throw err;
+
+  console.log(JSON.parse(results));
+})
 
 app.use(bodyParser.json());
 app.use('/', express.static('frontend'));
 
-const sampeTweets = fs.readFileSync('sample-tweets.json.txt', 'UTF-8');
-// let arrayOfJsons = sampeTweets.split('\n');
-// console.log(JSON.parse(arrayOfJsons[0]));
+function sleep(ms) {
+  var start = new Date().getTime(), expire = start + ms;
+  while (new Date().getTime() < expire) { }
+  return;
+}
+function randomNumber() {
+  return Math.floor(Math.random() * (4 - 0) + 0);
+}
+function selectTweet() {
+  const sampleTweets = fs.readFileSync('sample-tweets.json.txt', 'UTF-8').toString().split('\n');
+  return JSON.parse(sampleTweets[randomNumber()]);
+}
 
 app.post('/initial-tweet', function(req, res) {
   const userId = req.body.userId;
-  let newTweet = selectNextTweetCarefully(userId);
-  res.send(newTweet);
+  sleep(2000);
+  res.send(selectTweet());
 });
 
 app.post('/save-choice', function(req, res) {
   const userId = req.body.userId;
   const tweetId = req.body.tweetId;
   const choice = req.body.choice; // 1 or 0 or -1
-  
-  let newTweet = {
-    img: 'http://placehold.it/48',
-    name: 'سالم فلطة',
-    username: 'ok_nice@',
-    body: 'إعلان لصابون #دوف يظهر إمرأة سمراء البشرة و ملابس غامقة تتحول إليّ إمرأة بيضاء بعد استعمال منتجاتهم !تعليقكم ؟ كيف ترون الفكرة ؟',
-  };
-  const sampeTweets = fs.readFileSync('sample-tweets.json.txt', 'UTF-8');
-  let arrayOfJsons = sampeTweets.split('\n');
-  console.log(req.body.choice);
-  res.send(JSON.parse(arrayOfJsons[2]));
+  sleep(2000);
+  res.send(selectTweet());
 });
-
-function selectNextTweetCarefully(userId) {
-  return {
-    hi: 'hello'
-  };
-}
 
 app.listen(4000);
 console.log('APP IS LISTENING: http://localhost:4000');
